d383de1a6c92965aa96c8586cdfd880810174ce2
iancooper <ian_hammond_cooper@yahoo.co.uk>
2025-01-25 15:47:11 +0000

    fix: errors when saving to the Db caused by decimal precision

--- PizzaShop/StoreFrontCommon/PizzaShopDb.cs
+++ PizzaShop/StoreFrontCommon/PizzaShopDb.cs
@@ -6,4 +6,13 @@ public class PizzaShopDb(DbContextOptions<PizzaShopDb> options) : DbContext(opti
 {
     public DbSet<Order> Orders => Set<Order>();
     public DbSet<Topping> Toppings => Set<Topping>();
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Topping>()
+            .Property(t => t.Price)
+            .HasColumnType("decimal(18,2)");
+
+        base.OnModelCreating(modelBuilder);
+    }
 }
